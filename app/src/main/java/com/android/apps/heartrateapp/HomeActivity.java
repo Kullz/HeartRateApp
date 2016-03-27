cb4bdb1f684d49101f466f3923a1049a87c12ded
kull <kulianichev@gmail.com>
2016-03-27 11:27:06 +0300

    bitmap fixing

--- app/src/main/java/com/android/apps/heartrateapp/HomeActivity.java
+++ app/src/main/java/com/android/apps/heartrateapp/HomeActivity.java
@@ -1,21 +1,31 @@
 package com.android.apps.heartrateapp;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final float BYTES_PER_PX = 4.0f;
+
     private ImageButton arrowButton;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
         arrowButton = (ImageButton) findViewById(R.id.imageButton);
+        arrowButton.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.arrow, 82, 45));
         arrowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +35,43 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight){
+        //Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if(height > reqHeight || width > reqWidth){
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            //Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            //height and width larger that the requested height and width
+
+            while((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth){
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight){
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        //Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeResource(res, resId, options);
     }
 }
